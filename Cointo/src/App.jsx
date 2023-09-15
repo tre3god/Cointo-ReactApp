f253d665e91ceaf439d77e42343a4cd686d1d7cd
tre3 <goddywin@gmail.com>
2023-09-15 14:15:06 +0800

    done set up

--- Cointo/src/App.jsx
+++ Cointo/src/App.jsx
@@ -1,35 +1,19 @@
 import { useState, useEffect } from 'react'
+import { Route, Routes } from "react-router-dom";
 import './App.css'
+import Home from './Pages/Home/Home';
+import TrendingStore from './Stores/TrendingStore';
 
 export default function App() {
 
-  const [crypto, setCrypto] = useState([]);
-
-  async function fetchCoinsFromCoinGecko() {
-    try {
-      const response = await fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=true');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data from CoinGecko:', error);
-      return [];
-    }
-  }
-
-  useEffect(() => {
-  async function fetchData() {
-    const coinGeckoData = await fetchCoinsFromCoinGecko();
-    console.log(coinGeckoData); // You can do something with the fetched data here
-  }
-  fetchData();
-}, []);
 
   return (
       <>
-      <h1>Cointo</h1>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/trending" element={<TrendingStore />}></Route>
+
+      </Routes>
       </>
   )
 
---
+++ Cointo/src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import TrendingStore from '../../Stores/TrendingStore'
+
+
+export default function Home() {
+    
+  
+    
+
+  return (
+    <h1>Home</h1>  
+    
+
+)
+}
