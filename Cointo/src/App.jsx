3044f1614ee5ad4974fd12ceb91796b6fd3cea51
tre3 <goddywin@gmail.com>
2023-09-22 10:31:57 +0800

    changed homepage route

--- Cointo/src/App.jsx
+++ Cointo/src/App.jsx
@@ -11,12 +11,12 @@ export default function App() {
 
   return (
       <>
-      <Link to="/home" className="text-center text-6xl font-bold text-slate-100 bg-black p-5 block">
+      <Link to="/" className="text-center text-6xl font-bold text-slate-100 bg-black p-5 block">
         Cointo
       </Link>
       
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/trending/:id" element={<CoinPage />}></Route>
         <Route path="/portfolio" element={<Watchlist />}></Route>
       </Routes>
